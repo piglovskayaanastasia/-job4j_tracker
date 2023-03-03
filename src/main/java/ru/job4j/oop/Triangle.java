73d257a7af272dd374f05c4066e952393912a2e4
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2023-03-03 13:18:46 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642 #396932]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -17,7 +17,7 @@ public class Triangle {
 
     public boolean exist(double ab, double ac, double bc) {
         boolean rsl = false;
-        if ((ab + ac) > bc || (ab + bc) > ac || (bc + ac) > ab) {
+        if ((ab + bc) > ac || (ab + ac) > bc || (bc + ac) > ab) {
             rsl = true;
         }
         return rsl;
