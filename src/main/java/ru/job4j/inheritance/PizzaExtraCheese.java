4165aba2409f3c6c7abe76702a98222fbfe3286c
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2023-03-23 14:53:37 +0300

    6. Вызов переопределенного метода родителя - super.method ... [#504871]

---
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheese extends Pizza {
+
+    @Override
+    public String name() {
+        return "Just tasty pizza + extra cheese";
+    }
+}
