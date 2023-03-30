c91a367426015807d8718f6d06553b7f6814141b
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2023-03-30 19:06:01 +0300

    3. Массивы и модели. [#174892 #405006]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,12 +2,12 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book java1 = new Book("Clean code", 100);
+        Book cleanCode = new Book("Clean code", 100);
         Book java2 = new Book("Java2", 200);
         Book java3 = new Book("Java3", 200);
         Book java4 = new Book("Java4", 200);
         Book[] books = new Book[4];
-        books[0] = java1;
+        books[0] = cleanCode;
         books[1] = java2;
         books[2] = java3;
         books[3] = java4;
@@ -15,14 +15,20 @@ public class Library {
             Book bo = books[index];
             System.out.println(bo.getName() + ", " + bo.getPages());
         }
-        System.out.println("Rearrange the books with index 0 and 3");
-        Book temp = new Book("Java5", 200);
-        temp = books[0];
-        books[0] = books[2];
-        books[2] = temp;
+        System.out.println();
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
         for (int index = 0; index < books.length; index++) {
             Book bo = books[index];
             System.out.println(bo.getName() + ", " + bo.getPages());
+        }
+        System.out.println();
+        for (int index = 0; index < books.length; index++) {
+            Book bo = books[index];
+            if (bo.getName() == "Clean code") {
+                System.out.println(bo.getName() + ", " + bo.getPages());
+            }
         }
     }
 }
