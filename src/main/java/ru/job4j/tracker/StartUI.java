d0fc469f2bbe11baad52995b9eefd0e29569b780
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2023-05-25 21:52:37 +0300

    3.2. Реализация класса StartUI. Вывод всех заявок [#500745 #418574]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,8 +16,6 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select == 6) {
-                run = false;
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
@@ -26,28 +24,29 @@ public class StartUI {
                         System.out.println(item);
                     }
                 } else {
-                        System.out.println("Хранилище еще не содержит заявок");
+                    System.out.println("Хранилище еще не содержит заявок");
                 }
-
+            } else if (select == 6) {
+                run = false;
             }
         }
     }
 
-        private void showMenu() {
-            String[] menu = {
+    private void showMenu() {
+        String[] menu = {
                     "Add new Item", "Show all items", "Edit item",
                     "Delete item", "Find item by id", "Find items by name",
                     "Exit Program"
-            };
+        };
             System.out.println("Menu:");
             for (int i = 0; i < menu.length; i++) {
                 System.out.println(i + ". " + menu[i]);
             }
-        }
+    }
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             Tracker tracker = new Tracker();
             new StartUI().init(scanner, tracker);
-        }
+    }
 }
