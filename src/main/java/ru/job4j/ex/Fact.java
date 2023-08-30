f04fdd5e203ea53726b5ea73584d1d78e82cca4c
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2023-08-30 18:33:44 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -3,7 +3,7 @@ package ru.job4j.ex;
 public class Fact {
     public int calc(int n) {
         if (n > 0) {
-            throw new IllegalArgumentException("Start should be less than finish.");
+            throw new IllegalArgumentException("N could not be less than 0");
         }
         int rsl = 1;
         for (int index = 2; index <= n; index++) {
