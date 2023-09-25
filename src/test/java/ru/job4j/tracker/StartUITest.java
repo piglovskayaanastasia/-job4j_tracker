fa3e319040a4b8434ba4d50d771032672c6621a3
anastasiapiglovskaya <anastasiapiglovskaya@yandex.ru>
2023-09-25 19:13:35 +0300

    1.1. Тесты на StartUI. [#223313 #445393]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
@@ -57,16 +56,21 @@ public class StartUITest {
     public void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0"}
+                new String[] {"13", "0"}
         );
         Tracker tracker = new Tracker();
-        UserAction[] actions = {new ExitAction(out)
+        UserAction[] actions = new UserAction[] {
+                new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
         assertThat(out.toString()).isEqualTo(
-       "Menu." + System.lineSeparator()
-                       + "0. Exit" + System.lineSeparator()
+        "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "=== Exit Program ===" + ln
        );
-
     }
 }
